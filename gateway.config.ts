9bfcd36dbe09dc2593e7a234bc27ca9bc3517fb3
Denis Badurina <denis@denelop.com>
2025-09-04 21:19:25 +0200

    extract scopes and things

--- gateway.config.ts
+++ gateway.config.ts
@@ -51,9 +51,9 @@ export const gatewayConfig = defineConfig({
     mode: "protect-granular",
     // wont completely reject unauthenticated requests, but will not execute the field
     rejectUnauthenticated: false,
-    // extracts the user from the token and provides it for the `@requiresScopes` directive
-    resolveUserFn: (context: { jwt?: JWTExtendContextFields }) =>
-      context.jwt?.payload,
+    // resolve the user and extract the scopes from the token and provides it for the `@requiresScopes` directive
+    resolveUserFn: (context) => context.jwt?.payload,
+    extractScopes: (payload) => payload.scopes,
   },
   disableIntrospection: {
     // only disables introspection for non-admins
--- scripts/gen-jwt.ts
+++ scripts/gen-jwt.ts
@@ -4,7 +4,9 @@ import { JWT_SECRET } from "~env";
 const secretKey = JWT_SECRET;
 
 const payload = {
-  scopes: [],
+  scopes: ["admin"],
 };
 
-console.log(jwt.sign(payload, secretKey));
+const token = jwt.sign(payload, secretKey);
+console.log(token);
+console.log(JSON.stringify({ Authorization: `Bearer ${token}` }));
