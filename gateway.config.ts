99f5448fe56783479be1ad9110a01337cbd49730
Denis Badurina <denis@denelop.com>
2025-09-04 15:47:57 +0200

    otel in gw config

--- gateway.config.ts
+++ gateway.config.ts
@@ -1,8 +1,25 @@
-import "./telemetry";
-
 import { defineConfig } from "@graphql-hive/gateway";
+import { openTelemetrySetup } from "@graphql-hive/gateway/opentelemetry/setup";
+import { AsyncLocalStorageContextManager } from "@opentelemetry/context-async-hooks";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Logger } from "@graphql-hive/gateway";
+
+const log = new Logger();
+
+openTelemetrySetup({
+  log,
+  contextManager: new AsyncLocalStorageContextManager(),
+  traces: {
+    exporter: new OTLPTraceExporter({ url: "http://localhost:4318/v1/traces" }),
+  },
+  resource: {
+    serviceName: "graphql-conf-2025-gateway-workshop",
+    serviceVersion: "0.0.0",
+  },
+});
 
 export const gatewayConfig = defineConfig({
+  logging: log,
   openTelemetry: {
     traces: true,
   },
