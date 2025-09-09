0161f75cc039921cca6a6a0a660b606a485b3721
enisdenjo <badurinadenis@gmail.com>
2025-09-09 14:40:15 +0200

    compose

---
+++ mesh.config.ts
@@ -0,0 +1,21 @@
+import {
+  defineConfig,
+  loadGraphQLHTTPSubgraph,
+} from "@graphql-mesh/compose-cli";
+
+export const composeConfig = defineConfig({
+  subgraphs: [
+    {
+      sourceHandler: loadGraphQLHTTPSubgraph("users", {
+        endpoint: "http://localhost:4001/graphql",
+        source: "./subgraphs/users/typeDefs.graphql",
+      }),
+    },
+    {
+      sourceHandler: loadGraphQLHTTPSubgraph("posts", {
+        endpoint: "http://localhost:4002/graphql",
+        source: "./subgraphs/posts/typeDefs.graphql",
+      }),
+    },
+  ],
+});
