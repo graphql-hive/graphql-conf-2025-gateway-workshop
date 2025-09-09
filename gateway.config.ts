0cedd91bd34b3ab991b8df1112cdeaa22382f30b
enisdenjo <badurinadenis@gmail.com>
2025-09-09 14:51:18 +0200

    authenticated

--- gateway.config.ts
+++ gateway.config.ts
@@ -1,14 +1,23 @@
 import {
   createInlineSigningKeyProvider,
   defineConfig,
+  type JWTAuthContextExtension,
 } from "@graphql-hive/gateway";
 import { HMAC_SECRET, JWT_SECRET } from "./env";
 
-export const gatewayConfig = defineConfig({
+export const gatewayConfig = defineConfig<JWTAuthContextExtension>({
   jwt: {
     signingKeyProviders: [createInlineSigningKeyProvider(JWT_SECRET)],
+    reject: {
+      missingToken: false,
+    },
   },
   hmacSignature: {
     secret: HMAC_SECRET,
+  },
+  genericAuth: {
+    mode: "protect-granular",
+    resolveUserFn: (ctx) => ctx.jwt?.payload,
+    rejectUnauthenticated: false,
   },
 });
